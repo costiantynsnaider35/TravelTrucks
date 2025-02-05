fa644a1f3462e6bfc9654bfd008c7ef760216da2
Ваше ім'я <constantin161089@gmail.com>
2025-02-05 15:34:15 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import s from "./Hero.module.css";
 const Hero = () => {
   return (
     <div className={s.hero}>
-      <img src="/src/img/Picture.png" alt="Hero img" />
+      <img src="../../img/Picture.png" alt="Hero img" />
       <div className={s.heroSection}>
         <div className={s.heroTitle}>
           <h1 className={s.title}>Campers of your dreams</h1>
