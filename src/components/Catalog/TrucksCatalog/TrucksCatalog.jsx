47f8d9a3e7be20e6db70e992b0c7904849ad4ed9
Ваше ім'я <constantin161089@gmail.com>
2025-02-06 14:24:54 +0200

    del style

--- src/components/Catalog/TrucksCatalog/TrucksCatalog.jsx
+++ src/components/Catalog/TrucksCatalog/TrucksCatalog.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Trucks from "../Trucks/Trucks";
-import s from "./TrucksCatalog.module.css";
 
 const TrucksCatalog = () => {
   const [visibleItems, setVisibleItems] = useState(4);
@@ -9,10 +8,8 @@ const TrucksCatalog = () => {
     setVisibleItems((prevValue) => prevValue + 4);
   };
   return (
-    <div className={s.catalogContainer}>
-      <div className={s.camperContainer}>
-        <Trucks visibleItems={visibleItems} loadMoreItems={loadMoreItems} />
-      </div>
+    <div>
+      <Trucks visibleItems={visibleItems} loadMoreItems={loadMoreItems} />
     </div>
   );
 };
